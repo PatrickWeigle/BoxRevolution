fe947873433252a64605210f9f1243bc3233c06b
Douglas <douglas_ferreira95@hotmail.com>
2016-08-03 01:46:03 -0300

    Mira e outros

--- BoxCombat/BoxCombat/src/game/Cenario.java
+++ BoxCombat/BoxCombat/src/game/Cenario.java
@@ -1,4 +1,6 @@
 package game;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 
 import jplay.*;
@@ -6,12 +8,14 @@ public class Cenario {
 	private Window janela;
 	private Scene cena;
 	private Jogador jogador;
+	private Mira mira;
 	
 	public Cenario(Window window){
 		janela = window;
 		cena = new Scene();
 		cena.loadFromFile(URL.scenario("cenario.scn"));
 		jogador = new Jogador(1159, 520);
+		mira = new Mira(1159, 450);
 		
 		run();
 	}
@@ -22,7 +26,12 @@ public class Cenario {
 		while(true){
 			cena.draw();
 			jogador.draw();
+			mira.draw();
 			jogador.mover(janela);
+			mira.mover(janela, (int)jogador.getX()-90);
+			Color c = Color.getHSBColor(5f, 0.6f, 0.3f);
+			//janela.drawText("Jogador-1", 50, 690, c, new Font("Fipps", Font.PLAIN, 20));
+			janela.drawText("Jogador-2", 1050, 690, c, new Font("Fipps", Font.PLAIN, 20));
 			janela.update();
 			
 			if(input.keyDown(KeyEvent.VK_ESCAPE)){
